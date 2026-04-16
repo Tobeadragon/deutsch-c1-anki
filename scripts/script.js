178b5afad4dbea52ecf5fd67ad5e7fdd96960efc
Tobeadragon <mastertyj@hotmail.com>
2026-04-16 16:04:35 +0200

    modify deck name

--- scripts/script.js
+++ scripts/script.js
@@ -36,7 +36,7 @@ const App = {
         // 表示したい順番で定義
         const nameMap = {
             'FREE_SAMPLE': '🆓 無料サンプル',
-            'A1_FULL': '🇩🇪 ドイツ語 A1 100',
+            'A1_VOL1': '🇩🇪 ドイツ語 A1 VOL1',
             'A2_VOL1': '🇩🇪 ドイツ語 A2 VOL1',
             'B1_VOL1': '🇩🇪 ドイツ語 B1 VOL1',
             'C1_VOL1': '🇩🇪 ドイツ語 C1 VOL1',
